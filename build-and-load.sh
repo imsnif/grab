12c6afa4f6e6c9e7dbbd1e0c0857baf9af28a4a5
Aram Drevekenin <aram@poor.dev>
2025-10-22 09:45:04 +0200

    rename

--- build-and-load.sh
+++
@@ -1,2 +0,0 @@
-#cargo build && zellij plugin --skip-plugin-cache -- file:/home/aram/code/picker/target/wasm32-wasip1/debug/picker.wasm
-cargo build --release && cd /home/aram/code/zellij && /home/aram/backup/dev-zellij plugin --skip-plugin-cache -- file:/home/aram/code/picker/target/wasm32-wasip1/release/picker.wasm && cd /home/aram/code/picker
